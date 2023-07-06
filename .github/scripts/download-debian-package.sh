7817982eea35a99ada0a643d573d1135e994506c
Michael Hacker <mh@superbox.one>
2023-07-06 21:07:33 +0200

    Added -P to wget command

--- .github/scripts/download-debian-package.sh
+++ .github/scripts/download-debian-package.sh
@@ -3,6 +3,6 @@
 set -e
 
 mkdir -pv /tmp/dkms
-wget -O /tmp/dkms "https://repo.unipi.technology/debian/pool/main/u/unipi-kernel-modules/unipi-kernel-modules-dkms_${INPUT_DKMS-VERSION}_all.deb"
+wget -P /tmp/dkms "https://repo.unipi.technology/debian/pool/main/u/unipi-kernel-modules/unipi-kernel-modules-dkms_${INPUT_DKMS-VERSION}_all.deb"
 
 ls -la /tmp-dkms
