a829e1bf0b16c20e44954615435079415430aa5b
Michael Hacker <mh@superbox.one>
2023-07-06 21:52:42 +0200

    Check if commits and tags exists

--- .github/scripts/commit-kernel-modules.sh
+++ .github/scripts/commit-kernel-modules.sh
@@ -4,7 +4,14 @@ set -e
 
 git config --local user.email "github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
-git add modules
-git commit -m "Unipi Kernel Modules v${PACKAGE_VERSION}"
-git tag "v${PACKAGE_VERSION}"
+
+if [ "$(git status --porcelain | wc -l)" -gt "0" ]; then
+    git add modules
+    git commit -m "Unipi Kernel Modules v${PACKAGE_VERSION}"
+fi
+
+if ! [ "$(git tag -l "v${PACKAGE_VERSION}")" ]; then
+    git tag "v${PACKAGE_VERSION}"
+fi
+
 
