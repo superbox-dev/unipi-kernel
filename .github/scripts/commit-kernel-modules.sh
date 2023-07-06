e2248ff476024c5186525282cc2e018fb06b3af0
Michael Hacker <mh@superbox.one>
2023-07-06 22:24:54 +0200

    test latest tag

--- .github/scripts/commit-kernel-modules.sh
+++ .github/scripts/commit-kernel-modules.sh
@@ -5,13 +5,8 @@ set -e
 git config --local user.email "github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
 
-if ! [ "$(git tag -l "v${PACKAGE_VERSION}")" ]; then
-    if [ "$(git status --porcelain | wc -l)" -gt "0" ]; then
-        git add modules
-        git commit -m "Unipi Kernel Modules v${PACKAGE_VERSION}"
-    fi
-
-    git tag "v${PACKAGE_VERSION}"
+if [ "$(git status --porcelain | wc -l)" -gt "0" ]; then
+    git add modules
+    git commit -m "Unipi Kernel Modules v${PACKAGE_VERSION}"
 fi
-
 
