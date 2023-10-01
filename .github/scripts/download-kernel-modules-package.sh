66ab34cf28860b40de48193eb5be88e75799e04a
Michael Hacker <mh@superbox.one>
2023-10-01 20:15:29 +0200

    Fixed kernel deb name

--- .github/scripts/download-kernel-modules-package.sh
+++ .github/scripts/download-kernel-modules-package.sh
@@ -3,4 +3,4 @@
 set -e
 
 mkdir -pv /tmp/dkms
-wget -P /tmp/dkms "${REPO_URL}/${PACKAGE_NAME}_${PACKAGE_VERSION}_buster_all.deb"
+wget -P /tmp/dkms "${REPO_URL}/${PACKAGE_NAME}_${PACKAGE_VERSION}~buster_all.deb"
