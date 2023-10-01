6fd29bdc257923ef5c6236fcf8018f139253b6e9
Michael Hacker <mh@superbox.one>
2023-10-01 20:09:08 +0200

    Fix kernel url

--- .github/scripts/uncompress-kernel-modules-package.sh
+++ .github/scripts/uncompress-kernel-modules-package.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-ar x --output /tmp/dkms "/tmp/dkms/${PACKAGE_NAME}_${PACKAGE_VERSION}~bullseye_all.deb"
+ar x --output /tmp/dkms "/tmp/dkms/${PACKAGE_NAME}_${PACKAGE_VERSION}_buster_all.deb"
 tar xf /tmp/dkms/data.tar.xz -C /tmp/dkms
 
 if [ ! -d modules ]; then
   mkdir "${GITHUB_WORKSPACE}/modules"
 fi
 
-cp -R "/tmp/dkms/usr/src/unipi-${PACKAGE_VERSION}~bullseye/"* "${GITHUB_WORKSPACE}/modules"
+cp -R "/tmp/dkms/usr/src/unipi-${PACKAGE_VERSION}~buster/"* "${GITHUB_WORKSPACE}/modules"
 rm "${GITHUB_WORKSPACE}/modules/dkms.conf"
